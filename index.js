43d0dc3792069bbd3529ae505076ac6af68dbd81
Erika <erika@beasleys.com>
2020-09-05 17:09:57 -0400

    added functions to add dpartment, role, and employee

--- index.js
+++ index.js
@@ -41,25 +41,25 @@ function userData() {
         //Switch case set-up
         switch (result.choice) {
             case "Add Department":
-                addDepartment();
+                addDepartment()
                 break;
             case "Add Employee":
-                addEmployee();
+                addEmployee()
                 break;
             case "Add Role":
-                addRole();
+                addRole()
                 break;
             case "View all Departments":
-                viewDepartment();
+                viewDepartment()
                 break;
             case "View all Employees":
-                viewEmployee();
+                viewEmployee()
                 break;
             case "View all Roles":
-                viewRole();
+                viewRole()
                 break;
             case "Update Employee Role":
-                updateEmployee();
+                updateEmployee()
                 break;
             default:
                 connection.end()
@@ -67,4 +67,77 @@ function userData() {
         }
     })
 
+}
+
+//Function for Add Department
+function addDepartment() {
+    inquirer.prompt({
+        type: "input",
+        name: "department",
+        message: "What is the name of this Department?"
+    })
+    .then(function(res) {
+        connection.query('INSERT INTO Department (name) VALUES (?)', [res.department], function(err, data) {
+            if (err) throw err; 
+            console.log(`\n Successfully added ${res.department} as a department. \n`);
+            userData();
+        })
+    })
+}
+
+//Function for Add Employee
+function addEmployee() {
+    inquirer.prompt([{
+        type: "input",
+        name: "firstName",
+        message: "What is the First Name of this Employee?"
+    },
+    {
+        type: "input",
+        name: "lastName",
+        message: "What is the Last Name of this Employee?"
+    },
+    {
+        type: "input",
+        name: "roleId",
+        message: "What is the Role ID of this Employee?"
+    },
+    {
+        type: "input",
+        name: "managerId",
+        message: "What is the Manager's ID of this Employee?"
+    }])
+    .then(function(res) {
+        connection.query('INSERT INTO Employee (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)', [res.firstName, res.lastName, res.roleId, res.managerId], function(err, data) {
+            if (err) throw err; 
+            console.log(`\n Successfully added ${res.firstName} as an employee. \n`);
+            userData();
+        })
+    })
+}
+
+//Function for Add Role
+function addRole() {
+    inquirer.prompt([{
+        type: "input",
+        name: "title",
+        message: "What is the title of this role?"
+    },
+    {
+        type: "input",
+        name: "salary",
+        message: "What is the salary of this role?"
+    },
+    {
+        type: "input",
+        name: "departmentId",
+        message: "What is the department ID of this role?"
+    }])
+    .then(function(res) {
+        connection.query('INSERT INTO Role (title, salary, department_id) VALUES (?, ?, ?)', [res.title, res.salary, res.departId], function(err, data) {
+            if (err) throw err; 
+            console.log(`\n Successfully added ${res.title} as a role. \n`);
+            userData();
+        })
+    })
 }
